b49e6bbd9f5732c2dfaa41cf048eee7d4f120f7c
caiden <112859457+7bety@users.noreply.github.com>
2022-10-29 15:41:40 -0700

    Update whitelisted.lua

--- whitelisted.lua
+++ whitelisted.lua
@@ -9,6 +9,7 @@ whitelist = {
 [3299062613] = true, -- buyer
 [1864869334] = true, -- buyer
 [3568980282] = true, -- owner
-[3831053224] = true -- buyer
+[3831053224] = true, -- buyer
+[3680290273] = true -- buyer
 }
 return whitelist
